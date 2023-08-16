5ec10845adc6fcd04997c1e0c1f3e91262016746
Kurt Lupin Orioque <korioque@vertere-gs.com>
2023-08-17 04:05:35 +0800

    added firebase

---
+++ src/environments/firebase/demo.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABdX4xyIPsJ2zwEccZ43NTanxRDKNbaOk",
+  authDomain: "gym-management-demo.firebaseapp.com",
+  projectId: "gym-management-demo",
+  storageBucket: "gym-management-demo.appspot.com",
+  messagingSenderId: "911186991534",
+  appId: "1:911186991534:web:8085035130c2d9c0cc903f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
